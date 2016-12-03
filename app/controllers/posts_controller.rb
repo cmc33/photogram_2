54e7facb32f47b7dd60b03c19f77d7d2c7101038
cmc33 <cmcguigan33@gmail.com>
2016-12-03 13:06:13 -0500

    params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,14 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.create(post_params)
+  end
+
+  private
+
+def post_params  
+  params.require(:post).permit(:image, :caption)
+end  
+
 end
